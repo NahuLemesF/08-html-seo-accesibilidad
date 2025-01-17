49c9f6d180203e33d36e48cdfdfad1e281102d02
Nahuel Lemes <nahulem@gmail.com>
2025-01-17 08:51:52 -0300

    feat(styles): finalize styles of the project

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,20 @@
-// import './style.css'
-// import typescriptLogo from './typescript.svg'
-// import { setupCounter } from './counter.ts'
+document.addEventListener("DOMContentLoaded", () => {
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
+  const wrappers = document.querySelectorAll<HTMLAnchorElement>(".section__container--wrapper");
 
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  wrappers.forEach((wrapper) => {
+    const video = wrapper.querySelector<HTMLVideoElement>("video");
+
+    wrapper.addEventListener("mouseenter", () => {
+      if (video) {
+        video.play(); 
+      }
+    });
+
+    wrapper.addEventListener("mouseleave", () => {
+      if (video) {
+        video.pause(); 
+      }
+    });
+  });
+});
